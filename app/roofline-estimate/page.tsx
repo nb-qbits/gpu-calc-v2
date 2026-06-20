cd973ebdc6abd9c38dcf9c9b7a4947e28be6fff2
Amit Gambhir <gambhir.amit@gmail.com>
2026-06-20 08:33:30 -0400

    feat: add roofline estimate UI (PatternFly, client-side engine, no cost module)

--- app/roofline-estimate/page.tsx
+++ app/roofline-estimate/page.tsx
@@ -1,12 +1,5 @@
-import { PageSection, Title, Text, TextContent } from '@patternfly/react-core';
+import RooflineEstimate from './RooflineEstimate';
 
 export default function RooflineEstimatePage() {
-  return (
-    <PageSection>
-      <TextContent>
-        <Title headingLevel="h1" size="2xl">Roofline estimate</Title>
-        <Text>Coming soon.</Text>
-      </TextContent>
-    </PageSection>
-  );
+  return <RooflineEstimate />;
 }
