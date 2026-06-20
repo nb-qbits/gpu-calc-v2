6a72dad6b6ec3ac6b2df5f15a3d89add4d9a6862
Amit Gambhir <gambhir.amit@gmail.com>
2026-06-20 08:18:41 -0400

    feat: add roofline estimate nav item and stub page

---
+++ app/roofline-estimate/page.tsx
@@ -0,0 +1,12 @@
+import { PageSection, Title, Text, TextContent } from '@patternfly/react-core';
+
+export default function RooflineEstimatePage() {
+  return (
+    <PageSection>
+      <TextContent>
+        <Title headingLevel="h1" size="2xl">Roofline estimate</Title>
+        <Text>Coming soon.</Text>
+      </TextContent>
+    </PageSection>
+  );
+}
